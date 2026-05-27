a6259934df3748e185a977e73b210fc85c02b9c4
rdaudt <rdaudt@yahoo.com>
2026-05-26 17:08:43 -0700

    Simplify v1 nav by removing dashboard and templates tabs

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import { SignInPage } from './pages/SignInPage';
 import { ProtectedRoute } from './components/ProtectedRoute';
 import { PortalLayout } from './layout/PortalLayout';
-import { DashboardPage } from './pages/DashboardPage';
 import { BrandingPage } from './pages/BrandingPage';
-import { TemplatesPage } from './pages/TemplatesPage';
-import { TemplateEditorPage } from './pages/TemplateEditorPage';
 import { ClassLocationsPage } from './pages/ClassLocationsPage';
 import { ClassLocationEditorPage } from './pages/ClassLocationEditorPage';
 
@@ -21,11 +18,8 @@ export default function App() {
           </ProtectedRoute>
         )}
       >
-        <Route index element={<DashboardPage />} />
+        <Route index element={<Navigate to="/branding" replace />} />
         <Route path="branding" element={<BrandingPage />} />
-        <Route path="templates" element={<TemplatesPage />} />
-        <Route path="templates/new" element={<TemplateEditorPage />} />
-        <Route path="templates/:id" element={<TemplateEditorPage />} />
         <Route path="class-locations" element={<ClassLocationsPage />} />
         <Route path="class-locations/new" element={<ClassLocationEditorPage />} />
         <Route path="class-locations/:id" element={<ClassLocationEditorPage />} />
